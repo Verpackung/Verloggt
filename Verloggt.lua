70a8c28f3a225b9d34b1fd6e80bc6588fb136e2d
Verpackung <malsky_wow@web.de>
2024-10-22 18:11:14 +0200

    slash command and saved variables

--- Verloggt.lua
+++ Verloggt.lua
@@ -1,5 +1,3 @@
-message("Verloggt loaded..")
-
 -- TODO: 
 -- add all important spells
 -- color code spells in outop
@@ -9,6 +7,8 @@ message("Verloggt loaded..")
 -- check mrt timers
 -- maybe use something better for combat time
 -- spellnames instead of ids if it costs less performance
+-- scroll bar should only appear if content is too large
+SLASH_VERLOGGT1 = "/verloggt"
 
 -- 51505 lava burst, 188196 lightning bolt
 local spellsToLookFor = {51505, 188196}
@@ -18,6 +18,33 @@ local frame = CreateFrame("Frame")
 local currentTime
 local combatStartTime
 local inCombat = false
+VerloggtSettings = VerloggtSettings or {}
+
+local onMessage = "Verloggt is turned on!"
+local offMessage = "Verloggt is turned off!"
+
+local function printOnStatus()
+    if VerloggtSettings.isOn then
+        print(onMessage)
+    else
+        print(offMessage)
+    end
+
+end
+
+SlashCmdList["VERLOGGT"] = function(msg)
+    print(msg)
+    if msg == "status" then
+        printOnStatus()
+        return
+    else
+        VerloggtSettings.isOn = not VerloggtSettings.isOn
+    end
+    printOnStatus()
+
+end
+
+function GetSettings() return VerloggtSettings end
 
 -- Create a new frame
 local messageFrame = CreateFrame("ScrollingMessageFrame", "MyMessageFrame",
@@ -56,11 +83,8 @@ messageFrame:SetScript("OnDragStop", function(self)
     self:StopMovingOrSizing()
     print("OnDragStop")
 end)
-messageFrame.CloseButton:SetScript("OnClick", function()
-    messageFrame:Hide()
-    if messageList ~= nil then messageList:Clear() end
 
-end)
+messageFrame.CloseButton:SetScript("OnClick", function() messageFrame:Hide() end)
 
 -- Function to update the message list
 local function updateMessageList(messages)
@@ -80,11 +104,7 @@ local function updateMessageList(messages)
     messageFrame:SetSize(300, frameHeight + 40)
 end
 
-local function UpdateTime()
-    if inCombat then
-        currentTime = time() -- Current time in HH:MM:SS
-    end
-end
+local function UpdateTime() if inCombat then currentTime = time() end end
 
 local function StartCombatTimer()
     messageFrame:Hide()
@@ -134,11 +154,11 @@ end
 
 local function CombatTimeHandler(self, event, ...)
     if event == "PLAYER_REGEN_DISABLED" then
-        if inCombat == false then StartCombatTimer() end
+        if not inCombat then StartCombatTimer() end
         inCombat = true
         UpdateTime()
     elseif event == "PLAYER_REGEN_ENABLED" then
-        if inCombat == true then
+        if inCombat then
             inCombat = false
             print("combat timer stopped ")
             combatStartTime = nil
@@ -153,13 +173,10 @@ local function CombatLogHandler(self, event, ...)
           destName, _, _, spellID, spellName, spellSchool =
         CombatLogGetCurrentEventInfo()
 
-    -- timestamp instead of calculation
-
+    -- test if i can just use timestamp here
     if subEvent == "SPELL_CAST_SUCCESS" then
         UpdateTime()
         if contains(spellsToLookFor, spellID) then
-            local tmpCombatTime = GetCombatTimeStamp()
-            print("inserting: " .. spellName .. " - " .. tmpCombatTime)
             table.insert(recordedSpells,
                          SaveSpell(spellName, GetCombatTimeStamp(), sourceName))
         end
@@ -169,6 +186,14 @@ local function CombatLogHandler(self, event, ...)
 end
 
 local function EventHandler(self, event, ...)
+    if event == "ADDON_LOADED" then
+        message("Verloggt loaded..")
+        if VerloggtSettings.isOn == nil then VerloggtSettings.isOn = true end
+    end
+
+    -- do nothing if turned off
+    if not VerloggtSettings.isOn then return end
+
     if event == "COMBAT_LOG_EVENT_UNFILTERED" then
         CombatLogHandler(self, "COMBAT_LOG_EVENT_UNFILTERED", ...)
     end
@@ -180,5 +205,6 @@ end
 frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 frame:RegisterEvent("PLAYER_REGEN_DISABLED")
 frame:RegisterEvent("PLAYER_REGEN_ENABLED")
+frame:RegisterEvent("ADDON_LOADED")
 frame:SetScript("OnEvent", EventHandler)
 
